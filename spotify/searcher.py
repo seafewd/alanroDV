0a598250100a22721833febfc190eeb3e76ad529
alex <alexander.korpas@protonmail.com>
2020-08-19 17:57:53 +0200

    dsa

---
+++ spotify/searcher.py
@@ -0,0 +1,14 @@
+import spotipy.oauth2 as oauth2
+client_id = ""
+client_secret = ""
+
+credentials = oauth2.SpotifyClientCredentials(
+    client_id=client_id,
+    client_secret=client_secret)
+
+print(credentials)
+
+token = credentials.get_access_token()
+
+# Create new API object wrapper
+spotify = spotipy.Spotify(auth=token)
